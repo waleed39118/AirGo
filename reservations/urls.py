3aff1099dc81ee5c3ad8fb45e142e5da8cd14c24
waleed39118 <waleed.moltaka3@gmail.com>
2025-09-19 12:55:19 +0300

    adding files

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,19 +1,16 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .import views
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('search/', views.search, name='search'),
+    path('', views.welcome, name='welcome'),
     path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('home/', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
-    path('flight/<int:flight_id>/', views.book_flight, name='book_flight'),
+    path('book/<int:flight_id>/', views.book_flight, name='book_flight'),
     path('destination/<int:dest_id>/', views.destination_detail, name='destination_detail'),
     path('weather/<int:dest_id>/', views.weather, name='weather'),
     path('checkout/<int:booking_id>/', views.checkout, name='checkout'),
     path('create-checkout-session/<int:booking_id>/', views.create_checkout_session, name='create_checkout_session'),
-    path('success/<int:booking_id>/', views.success, name='success'),
-    path('cancel/', views.cancel, name='cancel'),
 ]
